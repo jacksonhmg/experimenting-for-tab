65c51321885951fb66f982d2cfd244444b2b109e
jacksonhmg <jacksonmowattgok@gmail.com>
2023-10-26 00:36:08 -0700

    button works (only if before notif)

--- AppKit/Sources/Features/Root/RootView.swift
+++ AppKit/Sources/Features/Root/RootView.swift
@@ -5,13 +5,18 @@ import SwiftUI
 import UserNotifications
 
 
+struct NotificationDetailView: View {
+    var body: some View {
+        Text("This is the Notification Detail View")
+            .padding()
+    }
+}
+
 // MARK: - RootView
-
-
-
-
 struct RootView: View {
   private let notificationDelegate = UNUserNotificationCenterDelegateAdaptor()
+  @State private var showNotificationDetail: Bool = false
+
 
       func requestNotificationPermission() {
           let center = UNUserNotificationCenter.current()
@@ -39,17 +44,39 @@ struct RootView: View {
   }
 
   var body: some View {
-      TabBarContainerView(
-        selectedIndex: viewStore.binding(get: \.rawValue, send: Root.Action.selectTab),
-        screen1: RecordingListScreenView(store: store.scope(state: \.recordingListScreen, action: Root.Action.recordingListScreen)),
-        screen2: RecordScreenView(store: store.scope(state: \.recordScreen, action: Root.Action.recordScreen)),
-        screen3: SettingsScreenView(store: store.scope(state: \.settingsScreen, action: Root.Action.settingsScreen))
-      )
-      .accentColor(.white)
-      .onAppear(perform: requestNotificationPermission)  // Add this line
-      .task { viewStore.send(.task) }
-      .enableInjection()
+      NavigationView {
+        VStack {
+          Button("Test Notification Detail") {
+              showNotificationDetail.toggle()
+          }
+          
+          ZStack {
+              TabBarContainerView(
+                  selectedIndex: viewStore.binding(get: \.rawValue, send: Root.Action.selectTab),
+                  screen1: RecordingListScreenView(store: store.scope(state: \.recordingListScreen, action: Root.Action.recordingListScreen)),
+                  screen2: RecordScreenView(store: store.scope(state: \.recordScreen, action: Root.Action.recordScreen)),
+                  screen3: SettingsScreenView(store: store.scope(state: \.settingsScreen, action: Root.Action.settingsScreen))
+              )
+              .accentColor(.white)
+              .onAppear(perform: requestNotificationPermission)
+              .task { viewStore.send(.task) }
+              .enableInjection()
+              
+              .sheet(isPresented: $showNotificationDetail) {
+                  NotificationDetailView()
+              }
+          }
+        }
+      }
+      .onReceive(NotificationCenter.default.publisher(for: Notification.Name("ShowNotificationDetail"))) { _ in
+          print("Received the notification in the view!")
+          showNotificationDetail = true
+      }
+      
   }
+
+
+
 
 }
 
@@ -60,12 +87,29 @@ class UNUserNotificationCenterDelegateAdaptor: NSObject, UNUserNotificationCente
   }
 
     
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        // Handle the tap action here, you can use NotificationCenter or other patterns
-        // to communicate with the rest of your app or simply print something
-        print("Notification tapped!")
-        completionHandler()
-    }
+  func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+      
+      // Accessing the notification content
+      let content = response.notification.request.content
+      
+      // Extracting the title, subtitle, and body of the notification
+      let title = content.title
+      let subtitle = content.subtitle
+      let body = content.body
+      
+      print("Title: \(title), Subtitle: \(subtitle), Body: \(body)")
+    
+      print("Tapped the notification!")
+      
+      // Post a custom notification to indicate that the notification was tapped
+      NotificationCenter.default.post(name: Notification.Name("ShowNotificationDetail"), object: nil)
+      
+      completionHandler()
+  }
+
+  
+  
+
 }
 
 
